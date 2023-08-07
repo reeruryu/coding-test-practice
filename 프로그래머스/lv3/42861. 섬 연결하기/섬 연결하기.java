076b5c33dc4619d04d351e0590711a71a55a9d62
ryureeru <87798704+reeruryu@users.noreply.github.com>
2023-08-07 20:38:36 +0900

    [level 3] Title: 섬 연결하기, Time: 14.31 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    
+    static int[] island;
+	public static int solution(int n, int[][] costs) {
+		int answer = 0;		
+		Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
+        
+		island = new int[n];
+		for (int i = 0; i < n; i++) {
+            island[i] = i;
+        }
+        
+		for (int[] cost: costs) {
+			int u = findUnion(cost[0]);
+			int v = findUnion(cost[1]);
+            System.out.println(cost[0] +" "+u+", "+cost[1]+" "+v);
+			if (u != v) {
+				answer += cost[2];
+				island[u] = v;
+			}
+				
+		}
+        
+		return answer;
+	}
+    
+    public static int findUnion(int u) {
+		if (island[u] == u) return u;
+		return findUnion(island[u]);
+	}
+}
