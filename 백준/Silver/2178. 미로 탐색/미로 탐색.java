c9153e3eee4b10ac2b89863242be3c8ee5d521b7
ryureeru <87798704+reeruryu@users.noreply.github.com>
2023-09-18 19:41:19 +0900

    [Silver I] Title: 미로 탐색, Time: 132 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,54 +1,51 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N, M;
+    static int[][] miro;
+    static boolean[][] visited;
+    static int[][] dirs = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        
-        boolean[][] visited = new boolean[N][M];
-        int[][] miro = new int[N][M];
+
+        N = Integer.parseInt(st.nextToken()); // 행
+        M = Integer.parseInt(st.nextToken()); // 열
+        miro = new int[N][M];
+        visited  = new boolean[N][M];
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
-            String tmp = st.nextToken();
-            int j = 0;
-            for (String s: tmp.split("")) {
-                miro[i][j++] = Integer.parseInt(s);
+            String tmp = br.readLine();
+            for (int j = 0; j < tmp.length(); j++) {
+                miro[i][j] = tmp.charAt(j) - '0';
             }
         }
-        
-        
+        bfs();
+    }
+
+    public static void bfs() {
         Queue<int[]> queue = new LinkedList<>();
-        
         queue.offer(new int[]{0, 0});
         visited[0][0] = true;
         while (!queue.isEmpty()) {
-            int[] xy = queue.poll();
-            
-            for (int[] d: dirs) { // 11
-                int newX = xy[0] + d[0];
-                int newY = xy[1] + d[1];
-                
-                if (newX < 0 || newY < 0 || newX > N - 1 || newY > M - 1) {
-                    continue;
-                }
-                
-                if (!visited[newX][newY] && miro[newX][newY] != 0) {
-                    queue.offer(new int[]{newX, newY});
-                    miro[newX][newY] = miro[xy[0]][xy[1]] + 1;
-                    visited[newX][newY] = true;
-                }
+            int[] cur = queue.poll();
+            if (cur[0] == N - 1 && cur[1] == M - 1) {
+                System.out.println(miro[cur[0]][cur[1]]);
+                return;
             }
-   
-        }
-        
-        System.out.println(miro[N - 1][M - 1]);
-        
-	}
 
+            for (int[] d : dirs) {
+                int newX = cur[0] + d[0];
+                int newY = cur[1] + d[1];
+
+                if (newX < 0 || newX >= N || newY < 0 || newY >= M) continue;
+                if (miro[newX][newY] <= 0 || visited[newX][newY]) continue;
+
+                miro[newX][newY] = miro[cur[0]][cur[1]] + 1;
+                queue.offer(new int[]{newX, newY});
+                visited[newX][newY] = true;
+
+            }
+        }
+    }
 }
