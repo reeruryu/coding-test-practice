f78df924d4754160ec242c9f30496a1fc738ae71
ryureeru <87798704+reeruryu@users.noreply.github.com>
2023-10-06 14:22:44 +0900

    [unrated] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
+++ 프로그래머스/unrated/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -1,17 +1,35 @@
 SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, 
-(CASE
- WHEN STATUS = 'SALE' THEN '판매중'
- WHEN STATUS = 'RESERVED' THEN '예약중'
- WHEN STATUS = 'DONE' THEN '거래완료'
- END
-) AS STATUS
+    (
+        CASE
+        WHEN STATUS = 'DONE' THEN '거래완료'
+        WHEN STATUS = 'RESERVED' THEN '예약중'
+        ELSE '판매중'
+        END
+    ) AS STATUS
 FROM USED_GOODS_BOARD
-WHERE CREATED_DATE = '2022-10-05'
+WHERE DATE_FORMAT(CREATED_DATE, '%Y-%m-%d') = '2022-10-05'
 ORDER BY BOARD_ID DESC
 
-# SELECT board_id,writer_id,title, price ,CASE WHEN status = 'SALE' THEN '판매중'
-# WHEN status = 'RESERVED' THEN '예약중' 
-# WHEN status = 'DONE' THEN '거래완료' END status
-# FROM used_goods_board
-# WHERE board_id IN(SELECT board_id FROM used_goods_board WHERE created_date = '2022-10-05')
-# ORDER BY board_id DESC
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT BOARD_ID, WRITER_ID, TITLE, PRICE, 
+# (CASE
+#  WHEN STATUS = 'SALE' THEN '판매중'
+#  WHEN STATUS = 'RESERVED' THEN '예약중'
+#  ELSE '거래완료'
+#  END
+# ) AS STATUS
+# FROM USED_GOODS_BOARD
+# WHERE CREATED_DATE = '2022-10-05'
+# ORDER BY BOARD_ID DESC
