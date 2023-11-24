cb6aa89aa8d56a6dcf4a8385668d16ea56397f0b
ryureeru <87798704+reeruryu@users.noreply.github.com>
2023-11-24 18:30:08 +0900

    [Silver II] Title: 연속합, Time: 264 ms, Memory: 23424 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n;
+    static int max = Integer.MIN_VALUE;
+    static int[] arr;
+    static int[] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        arr = new int[n];
+        dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = arr[0];
+        max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+
+    }
+}
